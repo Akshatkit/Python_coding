6bb44a1cc4f607acdf46436d1c0a5d678baf9582
Akshat Gupta <134906462+Akshatkit@users.noreply.github.com>
2025-08-22 17:40:34 +0000

    container config done

--- Basic.py
+++ Basic.py
@@ -39,14 +39,64 @@ a = a^b
 b = a^b
 a= a^b
 print(f"Swap Two Variable :\n a : {a} \n b: {b}" )
-'''
 
-# Write a program to check if a number is even or odd.
-'''
+
+# Write a program to check if a number is even or odd..
+
 num =  int(input("Enter the Number : "))
 if num%2 == 0:
     print(f"{num} is Even.")
 else:
     print(f"{num} is Odd." )
+
+# Reverse a String
+
+inputStr = input("Reverse String : ")
+
+print(inputStr[::-1])
+
+
+def StringRevers(s):
+    reversedstr = ""
+    for char in s:
+        reversedstr = char + reversedstr
+    return reversedstr
+
+inputStr = input("Reverse String : ")
+RevStr = StringRevers(inputStr)
+
+print(RevStr)
+
+
+
+# Check Palindrome
+
+def check_pelindrome(p):
+    newstr = str(p)
+    pelindrome_num = newstr[::-1]
+    return int(pelindrome_num)
+
+num  = int(input("Enter Number to Check Pelindrome or not : "))
+callfun = check_pelindrome(num)
+if callfun == num:
+    print(f"{num} is Pelindrome")
+else:
+    print(f"{num} is not Pelindrome")
+
+    
+
+def Check_pelindrom(p):
+    rev = 0
+    temp = p
+    while temp > 0:
+        digit =temp%10 
+        rev = rev *10 + digit
+        temp = temp//10
+    print(rev)
+
+
+Check_pelindrom(121)
+
 '''
+
 
